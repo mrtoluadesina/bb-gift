0b70ccb5addb6f8cfd15600607bcaa924c934079
Tolu Adesina <tadesina90@gmail.com>
2019-11-06 16:37:35 +0100

    Added the faq content to the faq utils

---
+++ src/utils/faq.js
@@ -0,0 +1,22 @@
+export const faq = [
+  {
+    question: "How much is a Voucher?",
+    answer:
+      "N30,000 gives you 1 entry, N50,000 gives you 6 entries and N100,000 voucher gives you 15 entries"
+  },
+  {
+    question: "What happens to my money if I don't win?",
+    answer:
+      "N30,000 gives you 1 entry, N50,000 gives you 6 entries and N100,000 voucher gives you 15 entries"
+  },
+  {
+    question: "How long is the voucher valid for?",
+    answer:
+      "You get to use the voucher to buy products from the store. We have the widest variety of products for your baby at the best prices brought in directly from manufacturers in the USA and Asia. e.g with a 50k voucher you buy 50k worth of products from our stores and website"
+  },
+  {
+    question: "When is the draw?",
+    answer:
+      "You get to use the voucher to buy products from the store. We have the widest variety of products for your baby at the best prices brought in directly from manufacturers in the USA and Asia. e.g with a 50k voucher you buy 50k worth of products from our stores and website"
+  }
+];
