44dd8d3c395f3ed6675c1e1d1f48e3fadda791a4
Tolu Adesina <tadesina90@gmail.com>
2019-11-06 15:37:26 +0100

    updated App.js with suspense and routing and imported Home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React, {Suspense, lazy} from 'react';
-import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
-import './App.css';
-import ErrorBoundary from './common/ErrorBoundary';
+import React, { Suspense } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
+import "./App.css";
+import ErrorBoundary from "./common/ErrorBoundary";
+import Home from "./components/Home";
 
 function App() {
   return (
     <ErrorBoundary>
-    <p>App</p>
+      <Router>
+        <Suspense fallback={<div>One moment please ...</div>}>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Redirect to="/" />
+          </Switch>
+        </Suspense>
+      </Router>
     </ErrorBoundary>
   );
 }
