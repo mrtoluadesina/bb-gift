917194fbdbceda7e3dfe38198496a27c472e06a4
Tolu Adesina <tadesina90@gmail.com>
2019-11-06 16:50:19 +0100

    imported Header and Footer in the App.js and added to entire app

--- src/App.js
+++ src/App.js
@@ -8,17 +8,21 @@ import {
 import "./App.scss";
 import ErrorBoundary from "./common/ErrorBoundary";
 import Home from "./pages/Home";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <ErrorBoundary>
       <Router>
+        <Header />
         <Suspense fallback={<div>One moment please ...</div>}>
           <Switch>
             <Route exact path="/" component={Home} />
             <Redirect to="/" />
           </Switch>
         </Suspense>
+        <Footer />
       </Router>
     </ErrorBoundary>
   );
