99a92e31dfbf1fd57b9c209812e54a4d9606e76b
Tolu Adesina <tadesina90@gmail.com>
2019-11-07 03:21:06 +0100

    Updated the GiftCard component with steps state

--- src/components/giftCard/index.js
+++ src/components/giftCard/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import { Link, useHistory } from "react-router-dom";
+import React, { useState } from "react";
 import useForm from "react-hook-form";
-import { Input, TextArea } from "../Input";
+import { connect } from "react-redux";
+import "./index.scss";
+import GiftCardForm from "./Form";
+import { requestGiftCard, createGiftCard } from "./redux/action";
 
 function Giftcard(props) {
-  const {register, errors, handleSubmit} = useForm();
+  const { register, errors, handleSubmit } = useForm();
+  const [step, setStep] = useState(1);
+  const [values, setValues] = useState({});
+
+  const handleChange = e =>
+    setValues({ ...values, [e.target.name]: e.target.value });
+
+  const onSubmit = () => {
+    console.log("submitted")
+    setStep(2)
+};
+
   return (
     <div className="app-form">
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="form">
         <div className="form-header">
           <h2>step by step guide</h2>
           <ul>
-            <li>Step 1: Fill our details of voucher beneficiary.</li>
+            <li>Step 1: Fill out details of voucher beneficiary.</li>
             <li>Step 2: Select the amount of vouchers and pay.</li>
             <li>
               Step 3: Leave a lovely message… We’ll email them a voucher for the
@@ -19,103 +32,31 @@ function Giftcard(props) {
             </li>
           </ul>
         </div>
-        <div className="giftcard-form">
-          <fieldset>
-            <legend>
-              Receipient's Name
-              <small>
-                <Link to="/login">multiple receipients?</Link>
-              </small>
-            </legend>
-            <Input
-              type="text"
-              placeholder="Enter your name ..."
-              name="name"
-              onChange={handleChange}
-              ref={register({
-                required: true,
-                pattern: {
-                  value: /^[a-zA-Z- ]{3,}$/i,
-                  message: "Names cannot contain numbers and special characters"
-                }
-              })}
-            />
-            <span style={{ color: "red", fontSize: "12px" }}>
-              {errors.name && errors.name.message}
-            </span>
-            <Input
-              type="text"
-              placeholder="Enter your phone number ..."
-              name="phone"
-              onChange={handleChange}
-              ref={register({
-                required: true,
-                pattern: {
-                  value: /^0[0-9]{10,10}$/m,
-                  message: "Invalid phone number"
-                }
-              })}
-            />
-            <span style={{ color: "red", fontSize: "12px" }}>
-              {errors.phone && errors.phone.message}
-            </span>
-            <Input
-              type="text"
-              placeholder="Enter your email"
-              name="email"
-              onChange={handleChange}
-              ref={register({
-                required: true,
-                pattern: {
-                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                  message: "Invalid Email Address"
-                }
-              })}
-            />
-            <span style={{ color: "red", fontSize: "12px" }}>
-              {errors.email && errors.email.message}
-            </span>
-          </fieldset>
-          <fieldset>
-            <legend>Amount</legend>
-            <Input
-              type="number"
-              placeholder="How much? ..."
-              name="amount"
-              onChange={handleChange}
-              ref={register({
-                required: true,
-                pattern: {
-                  value: /^[0-9]{4,}/i,
-                  message: "Enter a Valid Number"
-                }
-              })}
-            />
-            <span style={{ color: "red", fontSize: "12px" }}>
-              {errors.amount && errors.amount.message}
-            </span>
-          </fieldset>
-          <fieldset>
-            <legend>Message to Receipient</legend>
-            <TextArea
-              rows="5"
-              placeholder="Enter your message ..."
-              name="message"
-              onChange={handleChange}
-              ref={register({
-                required: true,
-                message: "You have to enter a message"
-              })}
-            />
-            <span style={{ color: "red", fontSize: "12px" }}>
-              {errors.message && errors.message.message}
-            </span>
-          </fieldset>
-          <input type="submit" className="btn orange-bg" value="Proceed" />
-        </div>
-      </form>
+        {step === 1 && 
+        <GiftCardForm
+          register={register}
+          errors={errors}
+          handleChange={handleChange}
+          handleSubmit={handleSubmit}
+          proceed={onSubmit}
+        />}
+        {step === 2 && "This is payment"}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Giftcard;
+const mapStateToProps = state => ({
+  ...state
+});
+
+const mapDispatchToProps = dispatch => ({
+  requestGiftCardHandler: payload => dispatch(requestGiftCard(payload)),
+  createGiftCardHandler: (payload, token) =>
+    dispatch(createGiftCard(payload, token))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Giftcard);
