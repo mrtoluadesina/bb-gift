ffe7a380d6a840f3d562212593905fa138278668
Tolu Adesina <tadesina90@gmail.com>
2019-11-07 23:24:06 +0100

    updated the faqs

--- src/utils/faq.js
+++ src/utils/faq.js
@@ -2,21 +2,21 @@ export const faq = [
   {
     question: "How much is a Voucher?",
     answer:
-      "N30,000 gives you 1 entry, N50,000 gives you 6 entries and N100,000 voucher gives you 15 entries"
-  },
-  {
-    question: "What happens to my money if I don't win?",
-    answer:
-      "N30,000 gives you 1 entry, N50,000 gives you 6 entries and N100,000 voucher gives you 15 entries"
+      "N30,000 gives you 1 entry, N50,000 voucher gives you 6 entries and N100,000 voucher gives you 15 entries."
   },
   {
     question: "How long is the voucher valid for?",
     answer:
-      "You get to use the voucher to buy products from the store. We have the widest variety of products for your baby at the best prices brought in directly from manufacturers in the USA and Asia. e.g with a 50k voucher you buy 50k worth of products from our stores and website"
+      "The voucher is valid for 1 year from the date of purchase."
+  },
+  {
+    question: "What happens to my money if I don't win?",
+    answer:
+      "You get to use the voucher to buy products from our store. we have the widest variety of products for your baby at the best prices brought in directly from manufacturers in USA and Asia. e.g with a 50k voucher, you buy 50k worth of products from our stores or website"
   },
   {
     question: "When is the draw?",
     answer:
-      "You get to use the voucher to buy products from the store. We have the widest variety of products for your baby at the best prices brought in directly from manufacturers in the USA and Asia. e.g with a 50k voucher you buy 50k worth of products from our stores and website"
+      "The draw would hold on Saturday 14th April, by 1pm at our Chevron Store: Folashade Jacobs street, Opposite Orchid Hotel, Dreamworld Africana road, After Chevron, Lekki and it would be streamed Live on our Instagram and facebook page."
   }
 ];
