1b99ef419996909126e5a7980b71901ebbf623d1
Tolu Adesina <tadesina90@gmail.com>
2019-11-07 16:10:31 +0100

    updated giftcard component with more functionalities and also made the form section reduce and enlarge with grace

--- src/components/giftCard/index.js
+++ src/components/giftCard/index.js
@@ -2,21 +2,89 @@ import React, { useState } from "react";
 import useForm from "react-hook-form";
 import { connect } from "react-redux";
 import "./index.scss";
+import {addTransition, removeTransition} from '../../helperFunctions/transition';
 import GiftCardForm from "./Form";
-import { requestGiftCard, createGiftCard } from "./redux/action";
+import GiftCardPayment from "./Payment";
+import GiftCardStatus from "./transactionStatus";
+import {
+  requestGiftCard,
+  createGiftCard,
+  handleChangeMethod
+} from "./redux/action";
 
 function Giftcard(props) {
   const { register, errors, handleSubmit } = useForm();
   const [step, setStep] = useState(1);
-  const [values, setValues] = useState({});
+  const [transactionStatus, setTransactionStatus] = useState("");
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    amount: "",
+    message: "",
+    senderName: "",
+    senderEmail: "",
+    senderPhone: ""
+  });
 
   const handleChange = e =>
     setValues({ ...values, [e.target.name]: e.target.value });
 
-  const onSubmit = () => {
-    console.log("submitted")
-    setStep(2)
-};
+  const onProceed = async () => {
+    const payload = {
+      sender: {
+        name: values.senderName,
+        email: values.senderEmail,
+        phone: values.senderPhone
+      },
+      amount: values.amount.toKobo(),
+      giftCards: [
+        {
+          message: values.message,
+          amount: values.amount.toKobo(),
+          recipient: {
+            name: values.senderName,
+            email: values.senderEmail,
+            phone: values.senderPhone
+          }
+        }
+      ]
+    };
+    await props.requestGiftCardHandler(payload);
+    addTransition('.giftcard-form','play-height');
+    setStep(2);
+  };
+
+  const goBack = () => (
+    <span className="back-btn"
+      onClick={e => {
+        e.preventDefault();
+        removeTransition('.giftcard-form','play-height');
+        setStep(step - 1);
+      }}
+    >
+      ⇠
+    </span>
+  );
+
+  const close = () => setStep(1);
+
+  const getReference = () => localStorage.getItem("transactionId");
+
+  const callback = async response => {
+    addTransition('.giftcard-form','play-height');
+    setStep(3);
+    const payload = {
+      transactionId: response.reference,
+      giftCards: props.giftCards
+    };
+    const result = await props.createGiftCardHandler(payload);
+    const { statusCode } = result;
+
+    statusCode > 201
+      ? setTransactionStatus("failed")
+      : setTransactionStatus("success");
+  };
 
   return (
     <div className="app-form">
@@ -32,25 +100,46 @@ function Giftcard(props) {
             </li>
           </ul>
         </div>
-        {step === 1 && 
-        <GiftCardForm
-          register={register}
-          errors={errors}
-          handleChange={handleChange}
-          handleSubmit={handleSubmit}
-          proceed={onSubmit}
-        />}
-        {step === 2 && "This is payment"}
+        <div className="giftcard-form">
+          {step > 1 ? goBack() : ""}
+          {step === 1 && (
+            <GiftCardForm
+              register={register}
+              errors={errors}
+              handleChange={handleChange}
+              handleSubmit={handleSubmit}
+              proceed={onProceed}
+            />
+          )}
+          {step === 2 && (
+            <GiftCardPayment
+              success={false}
+              callback={callback}
+              close={close}
+              reference={getReference()}
+              amount={+localStorage.getItem("totalAmount")}
+              email={values.senderEmail}
+            />
+          )}
+          {step === 3 && (
+            <GiftCardStatus>
+              <h3>Transaction {transactionStatus}</h3>
+            </GiftCardStatus>
+          )}
+        </div>
       </div>
     </div>
   );
 }
 
 const mapStateToProps = state => ({
-  ...state
+  isLoading: state.Giftcard.isLoading,
+  giftCards: state.Giftcard.giftCards
 });
 
 const mapDispatchToProps = dispatch => ({
+  giftCardInputChange: (index, name, value) =>
+    dispatch(handleChangeMethod(index, name, value)),
   requestGiftCardHandler: payload => dispatch(requestGiftCard(payload)),
   createGiftCardHandler: (payload, token) =>
     dispatch(createGiftCard(payload, token))
