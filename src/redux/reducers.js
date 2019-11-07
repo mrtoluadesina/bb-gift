22cd91488e928f0180bb94fed1a9578f99b504ca
Tolu Adesina <tadesina90@gmail.com>
2019-11-07 03:23:58 +0100

    updated the combined reducer with the Giftcard reducer

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,7 +1,8 @@
 import {combineReducers} from 'redux';
+import Giftcard from '../components/Giftcard/redux/reducer';
 
 const rootReducer = combineReducers({
-
+  Giftcard
 });
 
 export default rootReducer
