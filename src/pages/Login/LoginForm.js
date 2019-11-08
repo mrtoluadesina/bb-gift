6ec7d09391b46f2b6f87d17f8827543c432cc0f2
Tolu Adesina <tadesina90@gmail.com>
2019-11-08 01:44:33 +0100

    Login Form Created and validated with react hook form

--- src/pages/Login/LoginForm.js
+++ src/pages/Login/LoginForm.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import useForm from 'react-hook-form';
+import {Input} from '../../components/Input';
 
 export default function LoginForm(props) {
+  const {register, handleSubmit, errors} = useForm();
   return (
-    <h2>This is Login Form</h2>
+    <form onSubmit={handleSubmit(props.onSubmit)}>
+      <span style={{ color: "red", fontSize: "12px" }}>
+        {errors.email && errors.email.message}
+      </span>
+      <Input
+        type="text"
+        placeholder="Enter your email"
+        name="email"
+        onChange={props.handleChange}
+        ref={register({
+          required: true,
+          pattern: {
+            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+            message: "Invalid Email Address"
+          }
+        })}
+      />
+      <span style={{ color: "red", fontSize: "12px" }}>
+        {errors.password && errors.password.message}
+      </span>
+      <Input
+        type="password"
+        placeholder="Enter your password"
+        name="password"
+        onChange={props.handleChange}
+        ref={register({
+          required: true,
+          pattern: {
+            value: /^(?=.*[a-z])(?=.{8,})/g,
+            message: "Invalid Password - password needs to be atleast 8 characters remember"
+          }
+        })}
+      />
+      <input type="submit" className="btn orange-bg" value="Login" />
+    </form>
   )
 }
