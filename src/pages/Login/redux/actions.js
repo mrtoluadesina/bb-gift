3b672deb1f890a0d1e9714bd608b3950664261d1
Tolu Adesina <tadesina90@gmail.com>
2019-11-08 12:13:53 +0100

    Action update going  on - will  separating concerns currently

--- src/pages/Login/redux/actions.js
+++ src/pages/Login/redux/actions.js
@@ -3,7 +3,9 @@ import axios from 'axios';
 import {BASE_URL} from '../../../common/api';
 import setAuthorizationToken from '../../../helperFunctions/setAuthorizationToken';
 
-export const setUser = (token, isAdmin) => ({ type: types.SET_USER, token, isAdmin});
+export const setUser = (token, role) => ({ type: types.SET_USER, token, role});
+
+export const setAdminUser = (token, role) => ({ type: types.SET_AMIN_USER, token, role});
 
 export const loading = isLoading => ({type: types.SET_LOADING, isLoading});
 
@@ -13,11 +15,24 @@ export const login = payload => dispatch => {
   dispatch(loading(true));
   return axios
     .post(BASE_URL + "/auth/corporate_login", payload)
+    .then(res => {
+      const token = res.data.token;
+      const {type, _id, isCorporate} = res.data.payload;
+    })
+    .catch(error => {
+      
+    })
 }
 
 export const adminLogin = payload => dispatch => {
   dispatch(loading(true));
   return axios
     .post(BASE_URL + "/auth/admin_login", payload)
-    
+    .then(res => {
+      const token = res.data.token;
+      const {role, _id, isAdmin} = res.data.payload;
+    })
+    .catch(error => {
+
+    })
 }
