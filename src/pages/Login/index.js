713788664dc058aa28d98810dd8754f411c40a4d
Tolu Adesina <tadesina90@gmail.com>
2019-11-08 03:01:47 +0100

    Login component updated

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import LoginForm from './LoginForm';
+import {connect} from 'react-redux';
+import {SimpleCard} from '../../components/Card';
 
 function Login(props) {
   return (
+    <section className="auth-section">
     <LoginForm />
+    </section>
   )
 }
 
